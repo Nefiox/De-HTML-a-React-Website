36d1029c5dbb34cffaa8829433f555b39e9c9ebb
Juan Jiménez <nefioxdev@gmail.com>
2021-09-20 11:51:57 -0500

    Use of props & PropTypes in react

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,57 +1,26 @@
 import React from 'react';
 
-function ContentRowMovies() {
+function ContentRowMovies(props) {
     return(
-        <div class="row">
-
-            <div class="col-md-4 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-film fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-award fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
+        <div className="row">
+                {/*<!-- Movies in Data Base -->*/}
+            {props.blocks.map(block =>
+                <div className="col-md-4 mb-4">
+                    <div className={`card shadow h-100 py-2 ${block.border}`}>
+                        <div className="card-body">
+                            <div className="row no-gutters align-items-center">
+                                <div className="col mr-2">
+                                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">{block.title}</div>
+                                    <div className="h5 mb-0 font-weight-bold text-gray-800">{block.cifra}</div>
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-user fa-2x text-gray-300"></i>
+                                <div className="col-auto">
+                                    <i className={`fas ${block.icon} fa-2x text-gray-300`}></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ContentRowMovies from './ContentRowMovies';
 import LastMovieInDb from './LastMovieInDb';
 
+let stats = [
+    {
+    title: "Movies in Data Base",
+    cifra: 21,
+    border: "border-left-primary",
+    icon: "fa-film"
+},
+{
+    title: "Total awards",
+    cifra: 79,
+    border: "border-left-success",
+    icon: "fa-award"
+},
+{
+    title: "Actors quantity",
+    cifra: 49,
+    border: "border-left-warning",
+    icon: "fa-user"
+}
+];
+
 function ContentRowTop() {
     return (
         <div class="container-fluid">
@@ -9,7 +30,7 @@ function ContentRowTop() {
                 <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
             </div>
 
-            <ContentRowMovies />
+            <ContentRowMovies blocks = { stats } />
             <LastMovieInDb />
     </div>
     );
